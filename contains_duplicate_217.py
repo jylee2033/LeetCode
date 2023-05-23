78f1d827a50688264e1be37b652fa44cc1fedaae
jylee2033 <jylee2033@gmail.com>
2023-05-24 00:04:01 +0900

    217 solution 2

--- contains_duplicate_217.py
+++ contains_duplicate_217.py
@@ -3,11 +3,21 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        # solution 1
+        # return len(nums) != len(set(nums))
+
+        # solution 2
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
 
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4]
+    nums = [1, 2, 3, 1]
 
     solution = Solution()
     result = solution.containsDuplicate(nums)
