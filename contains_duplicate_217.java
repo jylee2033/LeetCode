be6bb5c0f4a1b8bc9f75865e317ddaa5c2e50c0a
jylee2033 <jylee2033@gmail.com>
2023-05-28 00:09:16 +0900

    contains duplicate java

---
+++ contains_duplicate_217.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    // solution 1 (time limit exceeded)
+    // for (int i = 0; i < nums.length; i++) {
+    // for (int j = i + 1; j < nums.length; j++) {
+    // if (nums[i] == nums[j]) {
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
+
+    // solution 2
+    Set<Integer> uniques = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (uniques.contains(nums[i])) {
+        return true;
+      }
+      uniques.add(nums[i]);
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 3, 1 };
+
+    Solution solution = new Solution();
+    boolean containsDuplicates = solution.containsDuplicate(nums);
+
+    System.out.println(containsDuplicates);
+  }
+}
