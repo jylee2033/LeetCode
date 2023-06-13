da8a6a0cdce01d2b20ef2f594d04ba4060d834e6
jylee2033 <jylee2033@gmail.com>
2023-06-13 21:08:27 +0900

    valid palindrome java

---
+++ Java/valid_palindrome_125.java
@@ -0,0 +1,40 @@
+package Java;
+
+class ValidPalindrome {
+  public boolean isPalindrome(String s) {
+    int i = 0;
+    int j = s.length() - 1;
+
+    while (i < j) {
+      Character start = s.charAt(i);
+      Character end = s.charAt(j);
+
+      if (!Character.isLetterOrDigit(start)) {
+        i++;
+        continue;
+      }
+
+      if (!Character.isLetterOrDigit(end)) {
+        j--;
+        continue;
+      }
+
+      if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+        return false;
+      }
+
+      i++;
+      j--;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    String s = "race a car";
+
+    ValidPalindrome solution = new ValidPalindrome();
+    boolean isPalindrome = solution.isPalindrome(s);
+
+    System.out.println(isPalindrome);
+  }
+}
