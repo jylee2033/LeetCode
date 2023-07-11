9877c7464bc43cc848ee6eeeae64bd69f0d42291
jylee2033 <jylee2033@gmail.com>
2023-07-11 23:48:28 +0900

    reverse linked list java

---
+++ Java/reverse_linked_list_206.java
@@ -0,0 +1,38 @@
+package Java;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class ReverseLinkedList {
+  public ListNode reverseList(ListNode head) {
+
+  }
+
+  public static void main(String[] args) {
+  }
+}
