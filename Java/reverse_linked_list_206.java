a5cafe4b910aff212c1fd79d0ae4d6729cb35dd8
jylee2033 <jylee2033@gmail.com>
2023-07-13 15:25:48 +0900

    reverse linked list java

--- Java/reverse_linked_list_206.java
+++ Java/reverse_linked_list_206.java
@@ -11,7 +11,7 @@ package Java;
  * }
  */
 
-public class ListNode {
+class ListNode {
   int val;
   ListNode next;
 
@@ -30,9 +30,33 @@ public class ListNode {
 
 class ReverseLinkedList {
   public ListNode reverseList(ListNode head) {
+    ListNode next = null;
+    ListNode previous = null;
+    ListNode current = head;
 
+    while (current != null) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+
+    return previous;
   }
 
   public static void main(String[] args) {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+
+    ReverseLinkedList solution = new ReverseLinkedList();
+    ListNode answer = solution.reverseList(head);
+
+    while (answer != null) {
+      System.out.println(answer.val);
+      answer = answer.next;
+    }
   }
 }
