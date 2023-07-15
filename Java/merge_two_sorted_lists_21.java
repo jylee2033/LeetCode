0620ee74297e2baba612ca8b94ef4793b5ef9662
jylee2033 <jylee2033@gmail.com>
2023-07-15 16:31:32 +0900

    merge two sorted lists java

---
+++ Java/merge_two_sorted_lists_21.java
@@ -0,0 +1,43 @@
+package Java;
+
+class MergeTwoSortedLists {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    final ListNode root = new ListNode();
+    ListNode mergedList = root;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        mergedList.next = list1;
+        list1 = list1.next;
+      } else {
+        mergedList.next = list2;
+        list2 = list2.next;
+      }
+
+      mergedList = mergedList.next;
+    }
+
+    if (list1 == null) {
+      mergedList.next = list2;
+    } else if (list2 == null) {
+      mergedList.next = list1;
+    }
+
+    return root.next;
+  }
+
+  public static void main(String[] args) {
+    ListNode list1 = new ListNode(1, new ListNode(2));
+    list1.next.next = new ListNode(3);
+    ListNode list2 = new ListNode(4, new ListNode(5));
+    list2.next.next = new ListNode(6);
+
+    MergeTwoSortedLists solution = new MergeTwoSortedLists();
+    ListNode answer = solution.mergeTwoLists(list1, list2);
+
+    while (answer != null) {
+      System.out.println(answer.val);
+      answer = answer.next;
+    }
+  }
+}
--- Java/reverse_linked_list_206.java
+++ Java/reverse_linked_list_206.java
@@ -45,8 +45,7 @@ class ReverseLinkedList {
   }
 
   public static void main(String[] args) {
-    ListNode head = new ListNode(1);
-    head.next = new ListNode(2);
+    ListNode head = new ListNode(1, new ListNode(2));
     head.next.next = new ListNode(3);
     head.next.next.next = new ListNode(4);
     head.next.next.next.next = new ListNode(5);
