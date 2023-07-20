41fee9c703a59632d6a0f892fd7b26ea482a1ac1
jylee2033 <jylee2033@gmail.com>
2023-07-20 23:59:00 +0900

    create test data

--- javaLeetcode/structure/TreeNode.java
+++ javaLeetcode/structure/TreeNode.java
@@ -18,4 +18,24 @@ public class TreeNode {
     this.left = left;
     this.right = right;
   }
+
+  // @param data [xx, null, xx]
+  // @return {@link TreeNode}
+  public static TreeNode createTestData(String data) {
+    if (data == null) {
+      return null;
+    }
+
+    data = data.substring(1, data.length() - 1);
+    String[] split = data.split(",");
+    int len = split.length;
+
+    TreeNode[] treeNodes = new TreeNode[len];
+
+    for (int i = 0; i < len; i++) {
+      if (split[i] != null) {
+        treeNodes[i] = new TreeNode(Integer.valueOf(split[i]));
+      }
+    }
+  }
 }
--- javaLeetcode/trees/_0226/Solution.java
+++ javaLeetcode/trees/_0226/Solution.java
@@ -15,7 +15,9 @@ public class Solution {
   }
 
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(4, new TreeNode(2), new TreeNode(7));
+    TreeNode root = new TreeNode(4);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(7);
     root.left.left = new TreeNode(1);
     root.left.right = new TreeNode(3);
     root.right.left = new TreeNode(6);
